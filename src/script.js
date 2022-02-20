70d0b0197bdc2e01ea407d469fe0242f75ada793
Maksym Mishchanchuk <yssam.nn@gmail.com>
2022-02-20 21:58:52 +0200

    lookbook_marka

--- src/script.js
+++ src/script.js
@@ -1,9 +1,11 @@
-let slides = document.querySelectorAll('#slides .slide');
+// eslint-disable-next-line strict
+const slides = document.querySelectorAll('#slides .slide');
 let currentSlide = 0;
-setInterval(nextSlide,3000);
+
+setInterval(nextSlide, 3000);
 
 function nextSlide() {
-    slides[currentSlide].className = 'slide';
-    currentSlide = (currentSlide + 1) % slides.length;
-    slides[currentSlide].className = 'slide showing';
+  slides[currentSlide].className = 'slide';
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].className = 'slide showing';
 }
